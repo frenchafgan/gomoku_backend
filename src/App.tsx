0a404f75663fca224bb94f4bc26783bb251ee75e
Mina Wanis <mwanis@turing.une.edu.au>
2023-08-26 19:39:58 +1000

    clear local storage on loading webpage

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,14 @@ import Routes from './Routes';
 import './styles/theme.css';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
+import React, { useEffect } from 'react';
 
-function App(): JSX.Element {
+const App: React.FC = () => {
+  useEffect(() => {
+    localStorage.clear();
+  }, []);
+
+
   return (
     <Provider store={store}>
       <div className="App">
