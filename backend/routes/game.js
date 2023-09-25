6da0bbd5063447a02c08ff129efc93c9dc6bb924
mwanis <mwanis@gmail.com>
2023-09-25 18:23:34 +1000

    improve Update a game logic

--- backend/routes/game.js
+++ backend/routes/game.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const Game = require('../models/Game');
+const authorize = require('../middleware/middleware'); // Import the authorization middleware
 
 // Create a new game
-router.post('/create', async (req, res) => {
+router.post('/create', authorize, async (req, res) => {
+  // Added authorize middleware
   const { id, boardSize, date, moves, result, username } = req.body;
   const newGame = new Game({ id, boardSize, date, moves, result, username });
   await newGame.save();
@@ -11,12 +13,19 @@ router.post('/create', async (req, res) => {
 });
 
 // Update a game
-router.put('/update/:gameId', (req, res) => {
+router.put('/update/:gameId', authorize, async (req, res) => {
+  // Added authorize middleware
   const { gameId } = req.params;
-  const { board } = req.body;
-  const game = games.find((g) => g.gameId === gameId);
-  if (game) {
-    game.board = board;
+  const { boardSize, date, moves, result, username } = req.body;
+
+  // Find and update the game in MongoDB
+  const updatedGame = await Game.findOneAndUpdate(
+    { id: gameId }, // Search by game ID
+    { boardSize, date, moves, result, username }, // Fields to update
+    { new: true } // Return the updated document
+  );
+
+  if (updatedGame) {
     res.status(200).send('Game updated');
   } else {
     res.status(404).send('Game not found');
