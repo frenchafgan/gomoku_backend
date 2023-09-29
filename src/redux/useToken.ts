4597c9d6c877c6bc02217bf7eefa92fa7f02387d
frenchafgan <mwanis@gmail.com>
2023-09-29 19:34:27 +1000

    Delete src directory

--- src/redux/useToken.ts
+++
@@ -1,8 +0,0 @@
-// useToken.ts
-import { useSelector } from 'react-redux';
-import { RootState } from '../redux/store';
-
-export const useToken = () => {
-  const token = useSelector((state: RootState) => state.auth.token);
-  return token;
-};
